a1efcb4801a69faa39dde55c465a5e9fdac26ca6
Folarin Lawal <lawalfolarin6@gmail.com>
2023-05-31 15:29:02 +0100

    done with raycaster and mouse events

--- script.js
+++ script.js
@@ -1,83 +1,44 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as dat from "lil-gui";
 import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
 
 /**
  * Base
  */
+// Initial gsap settings
+gsap.registerPlugin(SplitText);
+// gsap.set(".loader", { autoAlpha: 0 });
+let loaderText = new SplitText("#loadIndicator", { type: "chars" });
+
 // Debug
 const gui = new dat.GUI();
+gui.hide();
 
-// Loader
+// Loading manager
 const loadingManager = new THREE.LoadingManager();
-const dracoLoader = new DRACOLoader();
-// Add the decoder path to make it faster
-dracoLoader.setDecoderPath("/draco/");
-
-const gltfLoader = new GLTFLoader(loadingManager);
-gltfLoader.setDRACOLoader(dracoLoader);
-
-// Initialize mixer
-let mixer = null;
-
-// gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', (gltf) => {
-//  scene.add(gltf.scene)
-// });
-gltfLoader.load("/models/Fox/glTF/Fox.gltf", (gltf) => {
-  gltf.scene.scale.set(0.025, 0.025, 0.025);
-  scene.add(gltf.scene);
-
-  mixer = new THREE.AnimationMixer(gltf.scene);
-  const lookAround = mixer.clipAction(gltf.animations[0]);
-  const walk = mixer.clipAction(gltf.animations[1]);
-  const run = mixer.clipAction(gltf.animations[2]);
-  const params = {
-    gaze: () => {
-      walk.stop();
-      run.stop();
-      lookAround.play();
-    },
-    walk: () => {
-      walk.play();
-      lookAround.stop();
-      run.stop();
-    },
-    run: () => {
-      run.play();
-      walk.stop();
-      lookAround.stop();
-    },
-  };
-
-  const fox = gui.addFolder('Fox')
-
-  fox.add(params, "gaze").name('lookAround')
-  fox.add(params, "walk");
-  fox.add(params, "run");
-
-  // action.play();
-});
-
-// gltfLoader.load('/models/Duck/glTF-Draco/Duck.gltf', (gltf) => {
-//   scene.add(gltf.scene)
-// });
-let menuTL = gsap.timeline();
 
 loadingManager.onLoad = () => {
-  menuTL
-    .to(".loader h2", {
+  let loadTL = gsap.timeline();
+
+  loadTL
+    .to(loaderText.chars, {
+      yPercent: -50,
       autoAlpha: 0,
+      stagger: 0.03,
+      ease: "power4.inOut",
     })
     .to(".loader", {
-      yPercent: -100,
       delay: 1,
+      autoAlpha: 0,
       duration: 1,
-      ease: "power4.inOut",
     });
 };
+
+// Loaders
+const gltfLoader = new GLTFLoader(loadingManager);
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -85,37 +46,50 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// Lights
+const ambientLight = new THREE.AmbientLight("#ffffff", 0.3);
+
+const directionalLight = new THREE.DirectionalLight("#ffffff", 0.7);
+directionalLight.position.set(1, 2, 3);
+
+// Add Lights to the scene
+scene.add(ambientLight, directionalLight);
+
+// initialize Duck GLTF
+let duckGltf = null;
+
+// Load the model
+gltfLoader.load("./models/Duck/glTF-Binary/Duck.glb", (gltf) => {
+  duckGltf = gltf.scene;
+  duckGltf.position.y = -1.2;
+  //  Add the model to scene
+  scene.add(duckGltf);
+});
+
 /**
- * Floor
+ * Objects
  */
-const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(10, 10),
-  new THREE.MeshStandardMaterial({
-    color: "#444444",
-    metalness: 0,
-    roughness: 0.5,
-  })
+const object1 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: "#ff0000" })
 );
-floor.receiveShadow = true;
-floor.rotation.x = -Math.PI * 0.5;
-scene.add(floor);
+object1.position.x = -2;
 
-/**
- * Lights
- */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
-scene.add(ambientLight);
+const object2 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: "#ff0000" })
+);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(1024, 1024);
-directionalLight.shadow.camera.far = 15;
-directionalLight.shadow.camera.left = -7;
-directionalLight.shadow.camera.top = 7;
-directionalLight.shadow.camera.right = 7;
-directionalLight.shadow.camera.bottom = -7;
-directionalLight.position.set(5, 5, 5);
-scene.add(directionalLight);
+const object3 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: "#ff0000" })
+);
+object3.position.x = 2;
+
+// scene.add(object1, object2, object3);
+
+// Ray caster
+const raycaster = new THREE.Raycaster();
 
 /**
  * Sizes
@@ -139,6 +113,32 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+const mouse = new THREE.Vector2();
+
+//  windows events
+window.addEventListener("mousemove", (_event) => {
+  mouse.x = (_event.clientX / sizes.width) * 2 - 1;
+  mouse.y = (-_event.clientY / sizes.height) * 2 + 1;
+});
+
+// window.addEventListener("click", (_event) => {
+//   if (currentIntersect) {
+//     switch (currentIntersect.object) {
+//       case object1:
+//         console.log("Clicked Obj 1");
+//         break;
+//       case object2:
+//         console.log("Clicked Obj 2");
+//         break;
+//       case object3:
+//         console.log("Clicked Obj 3");
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+// });
+
 /**
  * Camera
  */
@@ -149,12 +149,11 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(2, 2, 2);
+camera.position.z = 3;
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
-controls.target.set(0, 0.75, 0);
 controls.enableDamping = true;
 
 /**
@@ -163,8 +162,6 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
@@ -172,16 +169,50 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Animate
  */
 const clock = new THREE.Clock();
-let previousTime = 0;
+
+// Witness
+let currentIntersect = null;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - previousTime;
-  previousTime = elapsedTime;
 
-  // update mixer
-  if (mixer !== null) {
-    mixer.update(deltaTime);
+  // Animate objects
+  // object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  // object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  // object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+  // Cast a ray
+
+  raycaster.setFromCamera(mouse, camera);
+
+  // const objectsToTest = [object1, object2, object3];
+  // const intersects = raycaster.intersectObjects(objectsToTest);
+
+  // objectsToTest.map((object) => {
+  //   object.material.color.set("#ff0000");
+  // });
+  // intersects.map((intersect) => {
+  //   intersect.object.material.color.set("#0000ff");
+  // });
+
+  // if (intersects.length) {
+  //   if (!currentIntersect) {
+  //     console.log("mouse enter");
+  //   }
+  //   currentIntersect = intersects[0];
+  // } else {
+  //   if (currentIntersect) {
+  //     console.log("mouse leave");
+  //   }
+  //   currentIntersect = null;
+  // }
+
+  if (duckGltf) {
+    const intersectDuck = raycaster.intersectObject(duckGltf);
+
+    intersectDuck.length
+      ? duckGltf.scale.set(1.2, 1.2, 1.2)
+      : duckGltf.scale.set(1, 1, 1);
   }
 
   // Update controls
