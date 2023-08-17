93c261d051027a28c168ed5c6f35a4ed344f3535
Folarin Lawal <lawalfolarin6@gmail.com>
2023-08-17 11:41:45 +0100

    working with sweet shaders

--- shaders/test/fragment.glsl
+++ shaders/test/fragment.glsl
@@ -1,5 +1,10 @@
 precision mediump float;
 
+// varying float vRandom;
+
 void main() {
-    gl_FragColor = vec4(0.9, 0.24, 0.24, 1.0);
+    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+
+    // gl_FragColor = vec4(0.5, vRandom * 0.5, 1.0, 1.0);
+
 }
--- shaders/test/vertex.glsl
+++ shaders/test/vertex.glsl
@@ -1,9 +1,33 @@
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+uniform vec2 uFrequency;
+
 
 attribute vec3 position;
+attribute float aRandom;
+
+// varying float vRandom;
+
+// float addFloat(float a, float b) {
+//     return a + b;
+// }
 
 void main() {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    // Floats and integers are different in glsl
+    // float sumOfTwo = addFloat(2.0, 4.1);
+
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.z += sin(modelPosition.x *uFrequency.x) * 0.1;
+    modelPosition.z += sin(modelPosition.y *uFrequency.y) * 0.1;
+    // modelPosition.z += aRandom * 0.1;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+
+    // vRandom = aRandom;
+
+    // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
