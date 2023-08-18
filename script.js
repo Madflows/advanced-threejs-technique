413ba4d56469a6b1f5d8c7c1f919cd309c3a5b24
Folarin Lawal <lawalfolarin6@gmail.com>
2023-08-19 00:35:44 +0100

    done with branch setup

--- script.js
+++ script.js
@@ -24,20 +24,20 @@ const scene = new THREE.Scene();
  */
 const loadingManager = new THREE.LoadingManager();
 const textureLoader = new THREE.TextureLoader(loadingManager);
-const flagTexture = textureLoader.load("/textures/nigeria.png");
+// const flagTexture = textureLoader.load("/textures/nigeria.png");
 
-loadingManager.onLoad = () => {
-  gsap
-    .timeline({
-      delay: 2
-    })
-    .to(".ld-content", {
-      autoAlpha: 0,
-    })
-    .to(".loader", {
-      y: "-100vh",
-    });
-};
+// loadingManager.onLoad = () => {
+//   gsap
+//     .timeline({
+//       delay: 2
+//     })
+//     .to(".ld-content", {
+//       autoAlpha: 0,
+//     })
+//     .to(".loader", {
+//       y: "-100vh",
+//     });
+// };
 
 /**
  * Test mesh
@@ -45,121 +45,74 @@ loadingManager.onLoad = () => {
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 
-const count = geometry.attributes.position.count;
-const randoms = new Float32Array(count);
-
-for (let i = 0; i < count; i++) {
-  randoms[i] = Math.random();
-}
-
-geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1));
-
-console.log(geometry);
-
 // Material
 const material = new THREE.ShaderMaterial({
   vertexShader: testVertexShader,
   fragmentShader: testFragmentShader,
-  uniforms: {
-    uFrequency: {
-      value: new THREE.Vector2(10, 5),
-    },
-    uTime: {
-      value: 0,
-    },
-    uColor: {
-      value: new THREE.Color("orange"),
-    },
-    uTexture: {
-      value: flagTexture,
-    },
-  },
-  // wireframe: true,
-});
-
-gui
-  .add(material.uniforms.uFrequency.value, "x")
-  .max(20)
-  .step(0.5)
-  .name("frequencyX");
-gui
-  .add(material.uniforms.uFrequency.value, "y")
-  .max(20)
-  .step(0.5)
-  .name("frequencyY");
+  side: THREE.DoubleSide
+})
 
 // Mesh
-const mesh = new THREE.Mesh(geometry, material);
-mesh.scale.y = 2 / 3;
-scene.add(mesh);
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.set(0.25, -0.25, 1);
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0.25, - 0.25, 1)
+scene.add(camera)
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
-const clock = new THREE.Clock();
+const tick = () =>
+{
+    // Update controls
+    controls.update()
 
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+    // Render
+    renderer.render(scene, camera)
 
-  // Update material
-  material.uniforms.uTime.value = elapsedTime;
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
 
-  // Update controls
-  controls.update();
-
-  // Render
-  renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
-
-tick();
+tick()
