1bcf5416cc538ec6fb8e30402cbf73326d19de4f
Folarin Lawal <lawalfolarin6@gmail.com>
2023-08-19 11:24:08 +0100

    learning shader patterns

--- shaders/test/fragment.glsl
+++ shaders/test/fragment.glsl
@@ -1,4 +1,24 @@
-void main()
-{
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+varying vec2 vUv;
+
+void main() {
+    // Pattern 3
+    // float strength = vUv.x;
+
+    // Pattern 4
+    // float strength = vUv.y;
+    
+    // Pattern 5
+    // float strength = 1.0 - vUv.y;
+   
+    // Pattern 6
+    // float strength = 10.0 * vUv.y;
+    
+    // Pattern 7
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    
+    // Pattern 8
+    float strength = mod(vUv.y * 10.0, 1.0);
+    strength = step(0.3, strength);
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
